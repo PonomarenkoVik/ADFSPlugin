92933ab062d03e5e4b751ea4eff6f396279d6d76
Viktor-Ponomarenko <Viktor.Ponomarenko@bd.com>
2022-08-11 12:11:55 +0300

    Add HTML form

---
+++ ADFSPlugin/PresentationForm .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityServer.Web.Authentication.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADFSPlugin
+{
+    internal class PresentationForm : IAdapterPresentationForm
+    {
+        public string GetFormHtml(int lcid) => Resources.HTMLForm;
+
+        public string GetFormPreRenderHtml(int lcid) => null;
+       
+        public string GetPageTitle(int lcid) => "ADFS Plugin";
+    }
+}
