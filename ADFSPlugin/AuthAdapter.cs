b8d167cf794f71c99f6395f4d6460a4e012b4796
Viktor-Ponomarenko <Viktor.Ponomarenko@bd.com>
2022-08-12 14:33:35 +0300

    Implement part of AuthAdapter logic

--- ADFSPlugin/AuthAdapter.cs
+++ ADFSPlugin/AuthAdapter.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.IdentityServer.Web.Authentication.External;
 using System;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Security.Claims;
 
@@ -7,36 +9,41 @@ namespace ADFSPlugin
 {
     public class AuthAdapter : IAuthenticationAdapter
     {
-        public IAuthenticationAdapterMetadata Metadata => throw new NotImplementedException();
+        public IAuthenticationAdapterMetadata Metadata => new Metadata();
 
         public IAdapterPresentation BeginAuthentication(Claim identityClaim, HttpListenerRequest request, IAuthenticationContext context)
         {
-            throw new NotImplementedException();
+            File.AppendAllLines("D:\\log.txt", new[] { $"BeginAuthentication:: identityClaim {identityClaim}, {identityClaim.Value}, request {request.Url} {request}, context {context.Data?.Select(x => x.Value)}" });
+            return new PresentationForm();
         }
 
         public bool IsAvailableForUser(Claim identityClaim, IAuthenticationContext context)
         {
-            throw new NotImplementedException();
+            File.AppendAllLines("D:\\log.txt", new[] { $"IsAvailableForUser:: identityClaim {identityClaim}, {identityClaim.Value}" });
+            return true;
         }
 
         public void OnAuthenticationPipelineLoad(IAuthenticationMethodConfigData configData)
         {
-            throw new NotImplementedException();
+            File.AppendAllLines("D:\\log.txt", new[] { $"OnAuthenticationPipelineLoad:: configData {configData}, {configData.Data}" });
         }
 
         public void OnAuthenticationPipelineUnload()
         {
-            throw new NotImplementedException();
+            File.AppendAllLines("D:\\log.txt", new[] { $"OnAuthenticationPipelineUnload" });
         }
 
         public IAdapterPresentation OnError(HttpListenerRequest request, ExternalAuthenticationException ex)
         {
-            throw new NotImplementedException();
+            File.AppendAllLines("D:\\log.txt", new[] { $"OnError:: request{request.QueryString} {request.Url}, ex {ex.Message}" });
+            return new PresentationForm();
         }
 
         public IAdapterPresentation TryEndAuthentication(IAuthenticationContext context, IProofData proofData, HttpListenerRequest request, out Claim[] claims)
         {
-            throw new NotImplementedException();
+            File.AppendAllLines("D:\\log.txt", new[] { $"TryEndAuthentication:: context{context.Data?.Select(x => x.Value)} {request.Url}, proofData {proofData.Properties}" });
+            claims = new Claim[0];
+            return new PresentationForm();
         }
     }
 }
