7bf606d7345e2743b57cdc8c75bc45c9598373f1
Viktor-Ponomarenko <Viktor.Ponomarenko@bd.com>
2022-08-12 20:12:44 +0300

    Correct PresentationForm

--- ADFSPlugin/PresentationForm .cs
+++ ADFSPlugin/PresentationForm .cs
@@ -7,9 +7,28 @@ using System.Threading.Tasks;
 
 namespace ADFSPlugin
 {
-    internal class PresentationForm : IAdapterPresentationForm
+    public class PresentationForm : IAdapterPresentationForm
     {
-        public string GetFormHtml(int lcid) => Resources.HTMLForm;
+
+        private readonly string _username;
+        private readonly Exception _exception;
+        public PresentationForm(string username)
+        {
+            _username = username;
+        }
+
+        public PresentationForm(string username, Exception ex) : this(username)
+        {
+            _exception = ex;
+        }
+
+        public string GetFormHtml(int lcid)
+        {
+            var form = _exception == null ? Resources.HTMLForm.Replace(Constants.PageIntroductionText, string.Empty) :
+                Resources.HTMLForm.Replace(Constants.PageIntroductionText, Resources.IncorrectPassword);
+
+            return form.Replace(Constants.Username, _username);
+        }
 
         public string GetFormPreRenderHtml(int lcid) => null;
        
